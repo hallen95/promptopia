8ea117afdef54f7657a8f81ab85a1915ad7485d7
Brian Ogas <bogas@Brians-MacBook-Pro.local>
2024-01-11 09:48:33 -0300

    perf: console.log added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -30,7 +30,7 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
-
+      console.log("data", data);
       setPosts(data);
     };
 
